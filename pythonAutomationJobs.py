4b0178f6380660bcd803fe7dc98d23ac6664cd66
ssnegi1971 <ssnegi@yahoo.com>
2025-11-10 07:13:49 +1100

    Add files via upload

---
+++ pythonAutomationJobs.py
@@ -0,0 +1,65 @@
+import sys
+import threading
+import subprocess
+
+#This is a Python Automation Parallel Framework. It runs python scripts in parallel.
+#It is programmed for maximum 5 python jobs in pythonJobs.txt seperated by a comma.
+#The sixth or last element is null inserted for collecting the previous subprocesses.
+#It can be expanded to more parallel jobs depending on requirement.
+
+file_path=sys.argv[1];
+
+def run_script(script_name):
+    subprocess.run(["python", script_name]);
+"""
+with open(file_path, 'r') as file:
+    file_content = file.read();
+print (file_content);
+split_list = file_content.split(',');
+print (split_list);
+"""
+lines = [];
+with open(file_path, "r") as file:
+    for line in file:
+        lines.append(line.strip());
+print(lines);
+for index, item in enumerate(lines):
+    print(f"Index: {index}, Item: {item}")
+    if __name__ == "__main__":
+        if index == 0 and item != 'null':
+            script_thread1 = threading.Thread(target=run_script, args=(item,));
+            script_thread1.start();
+        elif index == 1 and item != 'null':
+            script_thread2 = threading.Thread(target=run_script, args=(item,));
+            script_thread2.start();
+        elif index == 2 and item != 'null':
+            script_thread3 = threading.Thread(target=run_script, args=(item,));
+            script_thread3.start();
+        elif index == 3 and item != 'null':
+            script_thread4 = threading.Thread(target=run_script, args=(item,));
+            script_thread4.start();
+        elif index == 4 and item != 'null':
+            script_thread5 = threading.Thread(target=run_script, args=(item,));
+            script_thread5.start();
+        elif item == "null":
+            if index==1:
+                 script_thread1.join();
+            elif index == 2:
+                script_thread1.join();
+                script_thread2.join();
+            elif index == 3:
+                script_thread1.join();
+                script_thread2.join();
+                script_thread3.join();
+            elif index == 4:
+                script_thread1.join();
+                script_thread2.join();
+                script_thread3.join();
+                script_thread4.join();                
+            elif index == 5:
+                script_thread1.join();
+                script_thread2.join();
+                script_thread3.join();
+                script_thread4.join();
+                script_thread5.join();                
+            print("All scripts have finished executing.");
---
+++ pythonAutomationJobsMain.py
@@ -0,0 +1,39 @@
+import sys
+import threading
+import subprocess
+
+#This is the main file for Python Automation Parallel Framework.
+#It invokes the python automation script.
+#Multiple data files containing python jobs can be passed as a parameter.
+#Also multiple Main processes can be run for multiple sets of jobs each example different warehouses.
+
+def run_script(script_name, param1, param2):
+    subprocess.run(["python", script_name, param1, param2]);
+
+if __name__ == "__main__":
+    script_thread1 = threading.Thread(target=run_script, args=("C:/Users/ssneg/OneDrive/Desktop/work/Python/PythonAutomation/pythonAutomationJobs.py","C:/Users/ssneg/OneDrive/Desktop/work/Python/PythonAutomation/pythonJobs.txt","",));
+    script_thread1.start();
+    script_thread1.join();
+    script_thread1 = threading.Thread(target=run_script, args=("C:/Users/ssneg/OneDrive/Desktop/work/Python/PythonAutomation/SQLAlchemyPandasPythonProcedureCheckStatus.py","","",));
+    script_thread1.start();
+    script_thread1.join();
+    with open('C:/Users/ssneg/OneDrive/Desktop/work/Python/PythonAutomation/status.txt', 'r') as file:
+        file_content = file.read().rstrip('\n');
+        print(file_content);
+        if file_content == 'CONTINUE':
+            print("Next Task");
+            # Send email configuring your SMTP server.
+            script_thread1 = threading.Thread(target=run_script, args=("C:/Users/ssneg/OneDrive/Desktop/work/Python/PythonAutomation/sendmailoutlook.py","ssnegi@yahoo.com","Success",));
+            script_thread1.start();
+            script_thread1.join();
+            print ("email sent");
+        else:
+            print("Failure in Load");
+            # Send email configuring your SMTP server.
+            script_thread1 = threading.Thread(target=run_script, args=("C:/Users/ssneg/OneDrive/Desktop/work/Python/PythonAutomation/sendmailoutlook.py","ssnegi@yahoo.com","Failure",));
+            script_thread1.start();
+            script_thread1.join();
+            print ("email sent");
+            sys.exit(1);
+    print("second set of jobs to run");
+print("Main script run");
