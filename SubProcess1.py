d9d0cb325805da2874269df150dd58258db523ed
ssnegi1971 <ssnegi@yahoo.com>
2025-11-10 07:15:42 +1100

    Add files via upload

---
+++ SubProcess1.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.run(["python","SQLAlchemyPandasPythonProcedure1.py"]);
---
+++ SubProcess2.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.run(["python","SQLAlchemyPandasPythonProcedure2.py"]);
